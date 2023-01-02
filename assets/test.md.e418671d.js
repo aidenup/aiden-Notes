691b2b87b21591d4bd41414abe2c081473cc8e64
aidenup <aidenup@users.noreply.github.com>
2023-01-02 14:59:18 +0000

    deploy: 4db5fa3bb5cf445170843ab1a6501118755eedd4

--- assets/test.md.e418671d.js
+++ assets/test.md.e418671d.js
@@ -1,10 +1,11 @@
-import{_ as s,c as a,o as n,a as e}from"./app.2dc5d804.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"test.md"}'),t={name:"test.md"},l=e(`<div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-palenight"><code><span class="line"><span style="color:#A6ACCD;">function doSome() {</span></span>
+import{_ as s,c as a,o as n,a as e}from"./app.2dc5d804.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"test.md"}'),l={name:"test.md"},t=e(`<div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-palenight"><code><span class="line"><span style="color:#A6ACCD;">function doSome() {</span></span>
 <span class="line"><span style="color:#A6ACCD;">  // do some</span></span>
 <span class="line"><span style="color:#A6ACCD;">}</span></span>
 <span class="line"><span style="color:#A6ACCD;">aaa</span></span>
+<span class="line"><span style="color:#A6ACCD;">bbb</span></span>
 <span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div><nav class="table-of-contents"><ul></ul></nav><p><a href="/aiden-Notes/">Home</a></p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki material-palenight"><code><span class="line"><span style="color:#A6ACCD;">[foo](/foo/) &lt;!-- 跳转到 foo 文件夹的 index.html--&gt;</span></span>
 <span class="line"><span style="color:#A6ACCD;">[foo heading](./#heading) &lt;!-- 跳转到 foo/index.html 的特定标题位置 --&gt;</span></span>
 <span class="line"><span style="color:#A6ACCD;">[bar - three](../bar/three) &lt;!-- 你可以忽略扩展名 --&gt;</span></span>
 <span class="line"><span style="color:#A6ACCD;">[bar - three](../bar/three.md) &lt;!-- 具体文件可以使用 .md 结尾（推荐）--&gt;</span></span>
 <span class="line"><span style="color:#A6ACCD;">[bar - four](../bar/four.html) 也可以用 .html</span></span>
-<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,4),o=[l];function p(c,r,i,d,C,_){return n(),a("div",null,o)}const m=s(t,[["render",p]]);export{h as __pageData,m as default};
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,4),o=[t];function p(c,r,i,d,C,A){return n(),a("div",null,o)}const m=s(l,[["render",p]]);export{h as __pageData,m as default};
