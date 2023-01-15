87c732a805812befa7d3a5099f61828d209b8e15
aidenup <aidenup@users.noreply.github.com>
2023-01-15 15:11:25 +0000

    deploy: 4b55c0c16f3f3961f42439e9ceb0d99025cad0ac

---
+++ assets/essay_2022.md.ccaffe9d.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.23a9ea4d.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"essay/2022.md","lastUpdated":1673795445000}'),s={name:"essay/2022.md"};function r(o,c,n,p,_,d){return a(),t("div")}const f=e(s,[["render",r]]);export{m as __pageData,f as default};
