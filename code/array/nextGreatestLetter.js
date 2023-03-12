b2f8ebfa285186a0cfa41db5b48f9f92a0145180
aiden <danyang.aiden@outlook.com>
2023-03-12 10:06:56 +0800

    code

--- code/array/nextGreatestLetter.js
+++ code/array/nextGreatestLetter.js
@@ -1,3 +1,19 @@
 function nextGreatestLetter(letters, target) {
-
+  if (target > letters[letters.length - 1]) {
+    return letters[0]
+  }
+  let left = 0; right = letters.length - 1
+  while (left <= right) {
+    let mid = left + ((right - left) >> 1)
+    if (letters[mid] > target) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+  return letters[left]
 }
+
+const letters = ["c", "f", "j"], target = "a"
+const res = nextGreatestLetter(letters, target)
+console.log(res)
